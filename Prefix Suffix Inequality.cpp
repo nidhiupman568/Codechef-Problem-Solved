263e945e0c3423b2f59ea973e40c0f80dcac0f92
NIDHI UPMAN <130860182+nidhiupman568@users.noreply.github.com>
2024-06-18 07:26:07 +0530

    Create Prefix Suffix Inequality.cpp

---
+++ Prefix Suffix Inequality.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    int n;
+    cin >> n;
+    for(int i = 0; i<n-1; i++){
+        cout << 7 << ' ';
+    }
+    cout << 2 << '\n';
+    
+}
+int main() {
+	// your code goes here
+	ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    int t;
+    cin >> t;
+    while(t--) solve();
+}
